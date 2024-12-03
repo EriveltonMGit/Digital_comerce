d65b0a851cf75a0d56a5787d38d640417dbe12fe
EriveltonMGit <eriveltonprojetos.git@gmail.com>
2024-12-03 15:12:59 -0300

    first: build 00.6

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
- 
+
+// Obter o diretório equivalente ao __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
